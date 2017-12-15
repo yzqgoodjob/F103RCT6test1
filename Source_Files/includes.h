d6d981849ba606f5dd2145f85cc4e3c433840191
yzqgoodjob <yzq_goodjob@126.com>
2017-12-15 15:07:14 +0800

    add firstly

---
+++ Source_Files/includes.h
@@ -0,0 +1,19 @@
+#ifndef _INCLUDES_H_
+#define _INCLUDES_H_
+
+#include "init.h"
+#include "sys.h"
+#include "led.h"
+#include "delay.h"
+#include "usart.h"
+#include "lcd.h"
+#include "key.h"
+#include "time.h"
+#include "myiic.h"
+#include "24C02.h"
+#include "systemstart.h"
+#include "ultrasonic.h"
+#include "mymath.h"
+
+
+#endif
